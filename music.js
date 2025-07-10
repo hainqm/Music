d1d17f15634d7f572c5941d47c0444be97a82ee6
Hia <vh92588@gmail.com>
2025-07-10 17:17:27 +0700

    fix

--- music.js
+++ music.js
@@ -1,26 +1,5 @@
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 var progressBar = document.querySelector('.progress-bar');
 var progress = progressBar.querySelector('.progress');
 var progressSpan = progress.querySelector('span')
